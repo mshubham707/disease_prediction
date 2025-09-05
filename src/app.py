64a8c4b69f03fae50a2106be5d68a75358c5676d
mshubham707 <mshubham707@gmail.com>
2025-09-05 21:49:13 +0530

    Fix port for Hugging Face Spaces

--- src/app.py
+++ src/app.py
@@ -51,5 +51,5 @@ def predict():
     )
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Use PORT env var or default to 5000
+    port = int(os.environ.get("PORT", 7860))  # Use PORT env var or default to 5000
     app.run(host="0.0.0.0", port=port, debug=False)  # Bind to 0.0.0.0 for Spaces
